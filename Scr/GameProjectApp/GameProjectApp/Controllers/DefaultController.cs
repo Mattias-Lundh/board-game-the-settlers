2cb1735e6a42e923abc092cd1c7b8f4f87ab0ab5
Mattias-Lundh <31560859+Mattias-Lundh@users.noreply.github.com>
2018-01-18 17:13:31 +0100

    Add files via upload

---
+++ Scr/GameProjectApp/GameProjectApp/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameProjectApp.Models;
+
+namespace GameProjectApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ManageViewModels x = new ManageViewModels();
+            //HttpCookieCollection.
+            return View(x);
+        }
+    }
+}

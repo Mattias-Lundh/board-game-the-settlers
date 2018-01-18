9e7b6d60ca1fd622c605368258a235cb6f01687f
Mattias-Lundh <31560859+Mattias-Lundh@users.noreply.github.com>
2018-01-18 17:11:13 +0100

    Add files via upload

---
+++ Scr/GameProjectApp/GameProjectApp/Models/ManageViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameProjectApp.Models
+{
+    public class ManageViewModels
+    {
+        public string MyProperty { get; set; } = "hej";
+
+        public string MyFunction()
+        {
+            return MyProperty;
+        }
+    }
+}

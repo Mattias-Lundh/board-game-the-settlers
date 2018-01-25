433b6d24650505afb41a43d94d1d57f32afdac2e
matti <matti@LAPTOP-T1JAIQK0>
2018-01-25 17:44:44 +0100

    test example code

---
+++ Scr/GameProjectApp/GameEngineTest/PlayerActionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GameEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameEngineTest
+{
+    [TestClass]
+    public class PlayerActionTests
+    {
+        [TestMethod]
+        public void BuildRoad_BuildOneRoad_UpdatesGameModel()
+        {
+            // Arrange
+            GameInstruction gameInstruction = new GameInstruction();
+            GameStateModel game = new GameStateModel(gameInstruction);
+            //game.ActivePlayer = new Player("player1", "id1");
+
+            game.ActivePlayer.Inventory.Brick = 3;
+            PlayerAction pa = new PlayerAction(game);
+
+            // Act
+            pa.BuildRoad(23);
+
+            // Assert
+            Assert.AreEqual(2, game.ActivePlayer.Inventory.Brick);
+        }
+    }
+}
